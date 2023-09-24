6c860b7b792104d2bc99621d26511faebb3a9676
Swarga-codes <sd3526@srmist.edu.in>
2023-09-25 03:34:35 +0530

    added deploy configs

--- migrations/1_deploy_simple_storage.js
+++ migrations/1_deploy_simple_storage.js
@@ -1,5 +1,18 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
+// const SimpleStorage = artifacts.require("SimpleStorage");
 
-module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
-};
+// module.exports = function (deployer) {
+//   deployer.deploy(SimpleStorage);
+// };
+
+const fs=require('fs')
+const CarRentalPlatform=artifacts.require("CarRentalPlatform")
+
+module.exports=async function(deployer){
+await deployer.deploy(CarRentalPlatform);
+const instance=await CarRentalPlatform.deployed();
+let CarRentalPlatformAddress=await instance.address;
+let config="export const CarRentalPlatformAddress = "+CarRentalPlatformAddress;
+console.log("CarRentalPlatformAddress= "+CarRentalPlatformAddress);
+let data=JSON.stringify(config);
+fs.writeFileSync('config.js',JSON.parse(data))
+}
